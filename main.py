3092cb2a7b675a26656c4e4f97558daf99115ef8
Orangeliquid <hardym2020111@gmail.com>
2024-01-30 04:54:08 -0500

    Restructure code to utilize more classes

--- main.py
+++ main.py
@@ -1,93 +1,98 @@
 from tkinter import *
 import math
-# ---------------------------- CONSTANTS ------------------------------- #
-PINK = "#e2979c"
-RED = "#e7305b"
-GREEN = "#9bdeac"
-YELLOW = "#f7f5dd"
-FONT_NAME = "Courier"
-WORK_MIN = 25
-SHORT_BREAK_MIN = 5
-LONG_BREAK_MIN = 20
-reps = 0
-timer = None
-
-# ---------------------------- TIMER RESET ------------------------------- # 
 
 
-def reset_timer():
-    window.after_cancel(timer)
-    timer_label.config(text="Timer", bg=YELLOW, fg=GREEN, highlightthickness=0, font=(FONT_NAME, 35, "bold"))
-    checkmarks.config(text="")
-    canvas.itemconfig(timer_text, text="00:00")
-    global reps
-    reps = 0
-
-# ---------------------------- TIMER MECHANISM ------------------------------- #
-
-
-def start_timer():
-    global reps
-    reps += 1
-
-    work_sec = WORK_MIN * 60
-    short_break_sec = SHORT_BREAK_MIN * 60
-    long_break_sec = LONG_BREAK_MIN * 60
-
-    if reps % 8 == 0:
-        count_down(long_break_sec)
-        timer_label.config(text="Break", fg=RED, highlightthickness=0, font=(FONT_NAME, 35, "bold"))
-    elif reps % 2 == 0:
-        count_down(short_break_sec)
-        timer_label.config(text="Break", fg=PINK, highlightthickness=0, font=(FONT_NAME, 35, "bold"))
-    else:
-        count_down(work_sec)
-        timer_label.config(text="Work", fg=GREEN, highlightthickness=0, font=(FONT_NAME, 35, "bold"))
+class Constants:
+    PINK = "#e2979c"
+    RED = "#e7305b"
+    GREEN = "#9bdeac"
+    YELLOW = "#f7f5dd"
+    FONT_NAME = "Courier"
+    WORK_MIN = 25
+    SHORT_BREAK_MIN = 5
+    LONG_BREAK_MIN = 20
 
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
-def count_down(count):
-    count_min = math.floor(count / 60)
-    count_sec = count % 60
-    if count_sec < 10:
-        count_sec = f"0{count_sec}"
+class PomodoroTimer:
+    def __init__(self, window_setup):
+        self.window = window_setup
+        self.reps = 0
+        self.timer = None
+        self.timer_label = None
+        self.canvas = None
+        self.tomato_img = None
+        self.timer_text = None
+        self.checkmarks = None
+        self.start_button = None
+        self.reset_button = None
 
-    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
-    if count > 0:
-        global timer
-        timer = window.after(1000, count_down, count - 1)
-    else:
-        start_timer()
-        marks = ""
-        work_sessions = math.floor(reps/2)
-        for _ in range(work_sessions):
-            marks += "✔"
-        checkmarks.config(text=marks)
+    def reset_timer(self):
+        self.window.after_cancel(self.timer)
+        self.timer_label.config(text="Timer", bg=Constants.YELLOW, fg=Constants.GREEN, highlightthickness=0, font=(Constants.FONT_NAME, 35, "bold"))
+        self.checkmarks.config(text="")
+        self.canvas.itemconfig(self.timer_text, text="00:00")
+        self.reps = 0
+
+    def start_timer(self):
+        self.reps += 1
+
+        work_sec = Constants.WORK_MIN * 60
+        short_break_sec = Constants.SHORT_BREAK_MIN * 60
+        long_break_sec = Constants.LONG_BREAK_MIN * 60
+
+        if self.reps % 8 == 0:
+            self.count_down(long_break_sec)
+            self.timer_label.config(text="Break", fg=Constants.RED, highlightthickness=0, font=(Constants.FONT_NAME, 35, "bold"))
+        elif self.reps % 2 == 0:
+            self.count_down(short_break_sec)
+            self.timer_label.config(text="Break", fg=Constants.PINK, highlightthickness=0, font=(Constants.FONT_NAME, 35, "bold"))
+        else:
+            self.count_down(work_sec)
+            self.timer_label.config(text="Work", fg=Constants.GREEN, highlightthickness=0, font=(Constants.FONT_NAME, 35, "bold"))
+
+    def count_down(self, count):
+        count_min = math.floor(count / 60)
+        count_sec = count % 60
+        if count_sec < 10:
+            count_sec = f"0{count_sec}"
+
+        self.canvas.itemconfig(self.timer_text, text=f"{count_min}:{count_sec}")
+        if count > 0:
+            self.timer = self.window.after(1000, self.count_down, count - 1)
+        else:
+            self.start_timer()
+            marks = ""
+            work_sessions = math.floor(self.reps / 2)
+            for _ in range(work_sessions):
+                marks += "✔"
+            self.checkmarks.config(text=marks)
+
+    def ui_setup(self):
+        self.timer_label = Label(text="Timer", bg=Constants.YELLOW, fg=Constants.GREEN, highlightthickness=0, font=(Constants.FONT_NAME, 35, "bold"))
+        self.timer_label.grid(column=2, row=1)
+
+        self.canvas = Canvas(width=200, height=224, bg=Constants.YELLOW, highlightthickness=0)
+        self.tomato_img = PhotoImage(file="tomato.png")
+        self.canvas.create_image(100, 112, image=self.tomato_img)
+        self.timer_text = self.canvas.create_text(100, 130, text="00:00", fill="white", font=(Constants.FONT_NAME, 35, "bold"))
+        self.canvas.grid(column=2, row=2)
+
+        self.checkmarks = Label(bg=Constants.YELLOW, fg=Constants.GREEN, font=(Constants.FONT_NAME, 10, "bold"))
+        self.checkmarks.grid(column=2, row=4)
+
+        self.start_button = Button(text="Start", highlightthickness=0, command=self.start_timer, font=(Constants.FONT_NAME, 10, "bold"))
+        self.start_button.grid(column=1, row=3)
+
+        self.reset_button = Button(text="Reset", highlightthickness=0, command=self.reset_timer, font=(Constants.FONT_NAME, 10, "bold"))
+        self.reset_button.grid(column=3, row=3)
 
 
-# ---------------------------- UI SETUP ------------------------------- #
-window = Tk()
-window.title("Pomodoro")
-window.config(padx=100, pady=50, bg=YELLOW)
+if __name__ == "__main__":
+    window = Tk()
+    window.title("Pomodoro")
+    window.config(padx=100, pady=50, bg=Constants.YELLOW)
 
-timer_label = Label(text="Timer", bg=YELLOW, fg=GREEN, highlightthickness=0, font=(FONT_NAME, 35, "bold"))
-timer_label.grid(column=2, row=1)
+    pomodoro_timer = PomodoroTimer(window)
+    pomodoro_timer.ui_setup()
 
-canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
-tomato_img = PhotoImage(file="tomato.png")
-canvas.create_image(100, 112, image=tomato_img)
-timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.grid(column=2, row=2)
-
-
-checkmarks = Label(bg=YELLOW, fg=GREEN, font=(FONT_NAME, 10, "bold"))
-checkmarks.grid(column=2, row=4)
-
-start_button = Button(text="Start", highlightthickness=0, command=start_timer, font=(FONT_NAME, 10, "bold"))
-start_button.grid(column=1, row=3)
-
-reset_button = Button(text="Reset", highlightthickness=0, command=reset_timer, font=(FONT_NAME, 10, "bold"))
-reset_button.grid(column=3, row=3)
-
-
-window.mainloop()
+    window.mainloop()
